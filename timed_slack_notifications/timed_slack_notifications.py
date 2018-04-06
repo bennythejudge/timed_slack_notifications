0cd708a0876e7340bf43c5ac44d2149ee0e4d4dc
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2018-04-07 00:05:43 +0200

    Add dateutil

--- timed_slack_notifications/timed_slack_notifications.py
+++ timed_slack_notifications/timed_slack_notifications.py
@@ -1,24 +1,31 @@
 # main file
 import json
 from pprint import pprint
+import time
+import dateutil.parser as dp
 
 JSON_FILE = "timed_slack_notifications.json"
 
 def main():
+	# time now
+	ts = int(time.time())
+	print("ts: {}".format(ts))
+
 	# open the json file if it exists
 	read_json = json.load(open(JSON_FILE))
 
-	pprint(read_json)
+	# pprint(read_json)
 	# read it in memory
 	# loop through the content
 	# if 
-	print("content: {}".format(read_json["content"]))
-	print("total entries: {}".format(read_json["total entries"]))
+	# print("content: {}".format(read_json["content"]))
+	# print("total entries: {}".format(read_json["total entries"]))
 	items = read_json["items"]
-	print("items: {}".format(items))
+	# print("items: {}".format(items))
 	for item in items:
 		print(item)
-
+		entry_ts = item['timestamp']
+		print("entry_ts: {}".format(entry_ts))
 
 if __name__ == "__main__":
     main()
