ed2ef67acbe4530a2c7234fbf5ac6156c228c922
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2018-04-06 23:28:58 +0200

    Parse JSON

--- timed_slack_notifications/timed_slack_notifications.py
+++ timed_slack_notifications/timed_slack_notifications.py
@@ -1,17 +1,19 @@
 # main file
 import json
-
+from pprint import pprint
 
 JSON_FILE = "timed_slack_notifications.json"
 
 def main():
 	# open the json file if it exists
-	with open(JSON_FILE, 'r') as f:
-		read_json = json.load(f)
-	print(read_json)
+	read_json = json.load(open(JSON_FILE))
+
+	pprint(read_json)
 	# read it in memory
 	# loop through the content
 	# if 
+	print("content: {}".format(read_json["content"]))
+	print("total entries: {}".format(read_json["total entries"]))
 
 if __name__ == "__main__":
     main()
